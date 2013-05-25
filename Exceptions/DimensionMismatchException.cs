1107a4ede6ab5a899ee1e2076cb980cc98212cca
Alex Eyler <ane7887@rit.edu>
2013-05-25 17:09:59 -0400

    Added Exceptions

---
+++ Exceptions/DimensionMismatchException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLib {
+    public class DimensionMismatchException : System.ApplicationException {
+       
+        /* Each constructor just calls the ApplicationException constructor */
+        public DimensionMismatchException() : base() { }
+
+        /* Each constructor just calls the ApplicationException constructor */
+        public DimensionMismatchException(string message) : base(message) { }
+
+        /* Each constructor just calls the ApplicationException constructor */
+        public DimensionMismatchException(string message, System.Exception inner) : base(message, inner) { }
+    }
+}
---
+++ Exceptions/InvalidIndexException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MathLib {
+    /* InvalidIndexException class */
+    public class InvalidIndexException : System.ApplicationException {
+        /* Each constructor just calls the ApplicationException constructor */
+        public InvalidIndexException() : base() { }
+
+        /* Each constructor just calls the ApplicationException constructor */
+        public InvalidIndexException(string message) : base(message) { }
+
+        /* Each constructor just calls the ApplicationException constructor */
+        public InvalidIndexException(string message, System.Exception inner) : base(message, inner) { }
+    }
+}
